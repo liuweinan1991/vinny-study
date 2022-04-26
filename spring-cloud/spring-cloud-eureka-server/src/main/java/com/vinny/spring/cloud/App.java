76b07cba8caa50d940799d00cf0148fc9c98ad91
liuweinan <liuweinan@zsaipay.com>
2022-04-26 19:02:56 +0800

    11

---
+++ spring-cloud/spring-cloud-eureka-server/src/main/java/com/vinny/spring/cloud/App.java
@@ -0,0 +1,13 @@
+package com.vinny.spring.cloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
